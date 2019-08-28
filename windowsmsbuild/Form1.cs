f8d273ed427c7a72e06ec231bb66304a7abdd3e4
Abkjenkins <37396680+Abkjenkins@users.noreply.github.com>
2019-08-28 12:40:24 +0530

    Add files via upload

--- windowsmsbuild/Form1.cs
+++ windowsmsbuild/Form1.cs
@@ -20,7 +20,7 @@ namespace windowsmsbuild
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Test msbuild");
-            
+            sssss
          
         }
     }
